50487b25da1ccec9e1ca9eaf522afaada92d14a4
Manuel Corpas Kannen <Manuelkannen@gmail.com>
2024-06-08 15:54:37 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
 import appTemplate from './src/app/appTemplate';
 
-// Function to initialize the game after the template is loaded
 function initializeGame() {
-    // Get elements and set up event listeners
     const resultDiv = document.getElementById("result");
     const playerScoreSpan = document.getElementById("player-score");
     const computerScoreSpan = document.getElementById("computer-score");
@@ -12,7 +10,6 @@ function initializeGame() {
     const retryButton = document.getElementById("retry-button");
     const nextRoundButton = document.getElementById("next-round-button");
 
-    // Define other game variables and functions
     let playerScore = 0;
     let computerScore = 0;
     let countdownInterval;
@@ -28,14 +25,12 @@ function initializeGame() {
         choicesContainer.classList.remove("hidden");
         choicesContainer.classList.add("flex");
 
-        // Reset scores and update UI
         playerScore = 0;
         computerScore = 0;
         updateScores();
         resultDiv.textContent = "";
         countdownDisplay.classList.remove("hidden");
 
-        // Start countdown
         startCountdown(5);
     }
 
@@ -51,25 +46,20 @@ function initializeGame() {
     }
 
     function endGame() {
-        // Hide choices
         choicesContainer.classList.add("hidden");
         choicesContainer.classList.remove("flex");
 
-        // Generate computer choice and get result
         const computerChoice = getRandomChoice();
         const result = getResult(playerChoice, computerChoice);
 
-        // Display result and update scores
         resultDiv.textContent = result;
         resultDiv.classList.remove("hidden");
         countdownDisplay.classList.add("hidden");
         updateScores();
 
-        // Show retry and next round buttons
         retryButton.classList.remove("hidden");
         nextRoundButton.classList.remove("hidden");
 
-        // Hide player choices
         const choices = Array.from(choicesContainer.getElementsByTagName("button"));
         choices.forEach(choice => {
             choice.classList.add("hidden");
@@ -83,7 +73,7 @@ function initializeGame() {
 
     function getResult(player, computer) {
         if (!player) {
-            return "No se realizó ninguna selección. ¡El PC gana la ronda!";
+            return "No selection. PC wins the round!";
         } else if (player === computer) {
             return "Draw!";
         } else if (
@@ -92,10 +82,10 @@ function initializeGame() {
             (player === "scissors" && computer === "paper")
         ) {
             playerScore++;
-            return "¡HAS GANADO LA RONDA!";
+            return "You win the round!";
         } else {
             computerScore++;
-            return "¡HAS PERDIDO LA RONDA!";
+            return "You lose the round!";
         }
     }
 
@@ -105,7 +95,6 @@ function initializeGame() {
     }
 
     function resetGame() {
-        // Reset scores and UI
         playerScore = 0;
         computerScore = 0;
         updateScores();
@@ -114,47 +103,38 @@ function initializeGame() {
         resultDiv.textContent = "";
         resultDiv.classList.add("hidden");
 
-        // Show player choices
         const choices = Array.from(choicesContainer.getElementsByTagName("button"));
         choices.forEach(choice => {
             choice.classList.remove("selected", "hidden");
         });
 
-        // Clear countdown interval and hide countdown
         clearInterval(countdownInterval);
         countdownDisplay.classList.add("hidden");
 
-        // Show play button
         playButton.classList.remove("hidden");
         choicesContainer.classList.add("hidden");
         choicesContainer.classList.remove("flex");
     }
 
     function startNextRound() {
-      // Show player choices for next round
-      choicesContainer.classList.remove("hidden");
-      choicesContainer.classList.add("flex");
-      retryButton.classList.add("hidden");
-      nextRoundButton.classList.add("hidden");
-      resultDiv.textContent = "";
-      resultDiv.classList.add("hidden");
-      
-      // Clear previous countdown interval
-      clearInterval(countdownInterval);
-      
-      // Reset countdown display
-      countdownDisplay.classList.remove("hidden");
-      countdownDisplay.textContent = "5";
-      
-      // Reset player choices
-      const choices = Array.from(choicesContainer.getElementsByTagName("button"));
-      choices.forEach(choice => {
-          choice.classList.remove("selected", "hidden");
-      });
-  
-      // Start countdown for next round
-      startCountdown(5);
-  }
+        choicesContainer.classList.remove("hidden");
+        choicesContainer.classList.add("flex");
+        retryButton.classList.add("hidden");
+        nextRoundButton.classList.add("hidden");
+        resultDiv.textContent = "";
+        resultDiv.classList.add("hidden");
+
+        clearInterval(countdownInterval);
+        countdownDisplay.classList.remove("hidden");
+        countdownDisplay.textContent = "5";
+
+        const choices = Array.from(choicesContainer.getElementsByTagName("button"));
+        choices.forEach(choice => {
+            choice.classList.remove("selected", "hidden");
+        });
+
+        startCountdown(5);
+    }
 
     function startCountdown(seconds) {
         countdownDisplay.textContent = seconds;
@@ -170,11 +150,9 @@ function initializeGame() {
     }
 }
 
-// Wait for the DOM content to be loaded
 document.addEventListener('DOMContentLoaded', () => {
     const targetDiv = document.getElementById('game');
     targetDiv.innerHTML = appTemplate;
 
-    // Once the template is loaded, initialize the game
     initializeGame();
 });
